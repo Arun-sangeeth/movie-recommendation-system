7a008bab5eb244b20c0d350e1dc3c3d42a0f13df
Arun-sangeeth <arunsangeeth123@gmail.com>
2025-03-09 00:31:02 +0530

    Initial commit

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '456defb76952f064a2ad4336a40e519f';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '456defb76952f064a2ad4336a40e519f';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
